2d2c4a42c380aabffc5b1a91aa776815f35d35d3
Vahe <8384838z@gmail.com>
2024-12-13 02:12:00 +0400

    Implemented global file searching

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,3 +1,61 @@
+"use client";
+
+import { getFiles } from "@/lib/actions/file.action";
+import Image from "next/image";
+import Link from "next/link";
+import { useSearchParams } from "next/navigation";
+import { Models } from "node-appwrite";
+import { useEffect, useState } from "react";
+import { Input } from "./ui/input";
+
 export default function Search() {
-  return <div>Search</div>;
+  const [query, setQuery] = useState("");
+  const searchParams = useSearchParams();
+  const searchQuery = searchParams.get("query") || "";
+  const [results, setResults] = useState<Models.Document[]>([]);
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const fetchFiles = async () => {
+      const files = await getFiles({ query });
+      setResults(files.documents);
+      setOpen(true);
+    };
+    fetchFiles();
+  }, [query]);
+
+  useEffect(() => {
+    if (!searchQuery) {
+      setQuery("");
+    }
+  }, [searchQuery]);
+
+  return (
+    <div className="search">
+      <div className="search-input-wrapper">
+        <Image src="/icons/search.svg" alt="Search" width={15} height={15} />
+        <Input
+          type="text"
+          className="search-input"
+          placeholder="Поиск"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+
+        {open && (
+          <ul className="search-result">
+            {results.length > 0 ? (
+              results.map((file) => (
+                <li key={file.$id}>
+                  <Link href={`/files/${file.id}`}>{file.name}</Link>
+                </li>
+              ))
+            ) : (
+              <p className="empty-result">Ничего не найдено</p>
+            )}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
 }
